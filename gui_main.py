a42eabaebc5760e4d6a4e3bfaf00645d79868550
SparshSingh7586 <sparshsingh7586@gmail.com>
2023-06-02 15:03:30 +0530

    Implemented add operation. v2.1.0

--- gui_main.py
+++ gui_main.py
@@ -1,8 +1,25 @@
 import PySimpleGUI as gui
+import func
+import time
 
-label = gui.Text("Type in a todo: ")
-input_ = gui.InputText(tooltip='Enter todo')
-btn = gui.Button('Add')
-window = gui.Window('Sparsh Singh', layout=[[label, input_], [btn]])
-window.read()
+add_label = gui.Text('Enter todo: ')
+add_input = gui.InputText(tooltip='Enter todo', key='todo')
+add_btn = gui.Button('Add')
+
+window = gui.Window('Todo Application',
+                    layout=[[add_label], [add_input, add_btn]],
+                    font=('OCR A Extended', 20))
+
+while True:
+    event, value = window.read()
+    match event:
+        case 'Add':
+            todos = func.get_todos()
+            todo = value['todo']
+            num_of_todos = len(todos)
+            timestamp = time.strftime('%A %d %B %Y, %I:%M:%S %p')
+            todos[f'Todo_{str((num_of_todos + 1))}'] = {'message': todo, 'timestamp': timestamp}
+            func.write_todos(todos)
+            print('Added successfully')
+
 window.close()
