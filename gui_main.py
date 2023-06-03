c3cfa0d2a029bac87e657d4794860d85f8a9609c
SparshSingh7586 <sparshsingh7586@gmail.com>
2023-06-03 20:25:13 +0530

    Implemented complete operation v2.3.0

--- gui_main.py
+++ gui_main.py
@@ -5,6 +5,7 @@ import time
 add_label = gui.Text('Enter todo: ')
 add_input = gui.InputText(tooltip='Enter todo', key='todo')
 add_btn = gui.Button('Add')
+complete_btn = gui.Button('Complete')
 
 todo_listbox = gui.Listbox(values=[func.get_todos()[key]['message'] for key in func.get_todos()],
                            size=(40, 10),
@@ -13,7 +14,7 @@ todo_listbox = gui.Listbox(values=[func.get_todos()[key]['message'] for key in f
 edit_btn = gui.Button('Edit')
 
 window = gui.Window('Todo Application',
-                    layout=[[add_label], [add_input, add_btn], [todo_listbox, edit_btn]],
+                    layout=[[add_label], [add_input, add_btn], [todo_listbox, edit_btn, complete_btn]],
                     font=('OCR A Extended', 20))
 
 while True:
@@ -43,7 +44,17 @@ while True:
             window['todos'].update(values=[todos[key]['message'] for key in todos])
             func.write_todos(todos)
             print('Edited successfully')
-
+        case 'Complete':
+            todo_to_complete = value['todos'][0]
+            todos = func.get_todos()
+            for todo in todos.copy():
+                if todos[todo]['message'] == todo_to_complete:
+                    todos.pop(todo)
+            todos = func.rearrange_todos(todos)
+            func.write_todos(todos)
+            window['todos'].update(values=[todos[key]['message'] for key in todos])
+        case 'todos':
+            window['todo'].update(value=value['todos'][0])
         case gui.WIN_CLOSED:
             break
 
