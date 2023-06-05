ee3551572df27b9a00909f178f718d5cbb8f69e2
SparshSingh7586 <sparshsingh7586@gmail.com>
2023-06-05 18:39:53 +0530

    Implemented Add feature in web app. v4.1.0

--- web_main.py
+++ web_main.py
@@ -7,6 +7,17 @@ if not os.path.exists('todos.json'):
     with open('todos.json', 'w') as file:
         file.write('{}')
 
+
+def add_todo():
+    todo_local = st.session_state['new_todo']
+    todos_local = func.get_todos()
+    num_of_todos = len(todos)
+    timestamp = time.strftime('%A %d %B %Y, %I:%M:%S %p')
+    todos_local[f'Todo_{str((num_of_todos + 1))}'] = {'message': todo_local, 'timestamp': timestamp}
+    func.write_todos(todos_local)
+    print('Added successfully')
+
+
 st.title('My todo application')
 st.subheader('This application was made by sparsh singh')
 todos = func.get_todos()
@@ -15,4 +26,6 @@ for todo in todos:
     st.checkbox(todos[todo]['message'])
 
 st.text_input(label='',
-              placeholder='Add new todo...')
+              placeholder='Add new todo...',
+              key='new_todo',
+              on_change=add_todo)
